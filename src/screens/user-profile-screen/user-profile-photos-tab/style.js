4ed8743583fc1ee123531f3389a22c786cae2bfc
kaushikbhadarka <kaushikbhadarka99@gmail.com>
2020-11-23 22:32:36 +0530

    Imagepicker added and UI fixes

--- src/screens/user-profile-screen/user-profile-photos-tab/style.js
+++ src/screens/user-profile-screen/user-profile-photos-tab/style.js
@@ -5,4 +5,25 @@ export default StyleSheet.create({
   container: {
     flex: 1
   },
+  titleText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  textStyle: {
+    padding: 10,
+    color: 'black',
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#DDDDDD',
+    padding: 5,
+  },
+  imageStyle: {
+    width: 200,
+    height: 200,
+    margin: 5,
+  },
 });
