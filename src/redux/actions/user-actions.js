8be3cf6966eb9fd11e3c7f1837034f8786558d71
JagdishDer <jagdishder80@gmail.com>
2020-11-24 23:01:38 +0530

    Login and Register API calls

--- src/redux/actions/user-actions.js
+++ src/redux/actions/user-actions.js
@@ -2,21 +2,40 @@ import {ActionDispatcher} from './index';
 import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL} from './types';
 import {client} from '../../services/api-service';
 
-export const loginUser = (params) => (dispatch) =>
-  dispatch(ActionDispatcher(LOGIN_SUCCESS));
+export const registerUser = (requestData) => (dispatch) =>
+  // dispatch(ActionDispatcher(LOGIN_SUCCESS));
   // dispatch(ActionDispatcher(LOGIN_FAIL));
 
-  // new Promise(function (resolve, reject) {
-  //   dispatch(ActionDispatcher(LOGIN_REQUEST));
+  new Promise(function (resolve, reject) {
+    client
+      .post(`/registration`, requestData)
+      .then((res) => {
+        alert(JSON.stringify(res));
+        resolve(res);
+      })
+      .catch((err) => {
+        alert(JSON.stringify(err));
+        resolve({data: {success: false}});
+        reject(err);
+      });
+  });
 
-  //   client
-  //     .get(`/appsettings`)
-  //     .then((res) => {
-  //       alert(JSON.stringify(res));
-  //       resolve(res);
-  //     })
-  //     .catch((err) => {
-  //       alert(JSON.stringify(err));
-  //       reject(err);
-  //     });
-  // });
+export const loginUser = (requestData) => (dispatch) =>
+  // dispatch(ActionDispatcher(LOGIN_SUCCESS));
+  // dispatch(ActionDispatcher(LOGIN_FAIL));
+
+  new Promise(function (resolve, reject) {
+    dispatch(ActionDispatcher(LOGIN_REQUEST));
+    client
+      .post(`/login`, requestData)
+      .then((res) => {
+        alert(JSON.stringify(res));
+        resolve(res);
+      })
+      .catch((err) => {
+        dispatch(ActionDispatcher(LOGIN_FAIL));
+        alert(JSON.stringify(err));
+        resolve({data: {success: false}});
+        reject(err);
+      });
+  });
